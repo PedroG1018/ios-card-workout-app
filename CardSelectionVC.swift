f2e5fd81c722a9a40f7bb29a411e19fa9e80ea92
Pedro Gutierrez Jr <pedrogutie0@gmail.com>
2023-08-25 14:08:23 -0500

    set up button outlets, UI for rules screen

--- CardSelectionVC.swift
+++ CardSelectionVC.swift
@@ -11,18 +11,22 @@ class CardSelectionVC: UIViewController {
 
     @IBOutlet var cardImageView: UIImageView!
     
-    @IBOutlet var stopButton: UIButton!
-    @IBOutlet var rulesButton: UIButton!
-    @IBOutlet var restartButton: UIButton!
+    @IBOutlet var buttons: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        stopButton.layer.cornerRadius = 8
-        restartButton.layer.cornerRadius = 8
-        rulesButton.layer.cornerRadius = 8
+        for button in buttons {
+            button.layer.cornerRadius = 8
+        }
     }
 
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
     }
 }
