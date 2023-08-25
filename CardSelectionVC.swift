383bcd52ca9507787c8224f87b3b409c7cf961fe
Pedro Gutierrez Jr <pedrogutie0@gmail.com>
2023-08-25 14:28:19 -0500

    funcionality implemented, cards change based on timer, can stop and restart timer

--- CardSelectionVC.swift
+++ CardSelectionVC.swift
@@ -10,21 +10,42 @@ import UIKit
 class CardSelectionVC: UIViewController {
 
     @IBOutlet var cardImageView: UIImageView!
-    
     @IBOutlet var buttons: [UIButton]!
+    
+    var cards: [UIImage] = Card.allValues
+    
+    var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        startTimer()
         
         for button in buttons {
             button.layer.cornerRadius = 8
         }
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomCard), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomCard() {
+        let cardImage = cards.randomElement() ?? UIImage(named: "AS")
+        cardImageView.image = cardImage
+    }
 
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
     }
     
     @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
     }
     
     @IBAction func rulesButtonTapped(_ sender: UIButton) {
